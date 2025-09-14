2f056c3c545ec0be955ffa77957d973d56290f22
zzzawoyzzz <www.awoy@gmail..com>
2025-09-14 20:16:00 +0700

    format deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,8 @@ else
   echo ".env file not found!"
   exit 1
 fi
-
-
 gcloud config set account $ACCOUNT_ID
 gcloud config set project $PROJECT_ID
-
-
-
 gcloud app deploy app.yaml  --project=$PROJECT_ID --quiet
 gcloud app browse
 
